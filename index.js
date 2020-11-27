3d5a372381342ec109b016cbbdf1d507beb0daeb
intercept6 <4459220+intercept6@users.noreply.github.com>
2020-11-27 19:03:57 +0900

    🎉 initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const os = require('os')
+
+const getCpuCount = () => {
+  console.log(`CPU Count: ${os.cpus().length}`)
+}
+
+setInterval(getCpuCount, 3000)
+
+process.on('SIGINT', function onSigint () {
+  console.info('Got SIGINT (aka ctrl-c in docker). Graceful shutdown ', new Date().toISOString());
+  process.exit();
+});
+
+process.on('SIGTERM', () => {
+  console.info('Got SIGTERM (docker container stop). Graceful shutdown ', new Date().toISOString());
+  process.exit();
+})
